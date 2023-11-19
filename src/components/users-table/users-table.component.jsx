fd0575700cacb20f68791b00b5d23d29227c029b
mittal0802 <mittalkeshav41@gmail.com>
2023-11-19 16:20:57 +0530

    added search box, search results

--- src/components/users-table/users-table.component.jsx
+++ src/components/users-table/users-table.component.jsx
@@ -1,14 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserDataContext } from "../../contexts/users.context";
 import "./users-table.styles.scss";
 import SearchBox from "../search-box/search-box.component";
 const UsersTable = () => {
   const { usersData } = useContext(UserDataContext);
+  const [searchData, setSearchData] = useState(usersData);
+  const [searchField, setSearchField] = useState("");
+  const onSubmitChange = (event) => {
+    if (event.key === "Enter" || event.target.value.length === 0) {
+      const searchString = event.target.value.toLocaleLowerCase();
+      setSearchField(searchString);
+    }
+  };
+  useEffect(() => {
+    if (searchField.length === 0) return;
+    const newfilteredProducts = usersData.filter((user) =>
+      (user.username + user.phone + user.email + user.createdAt)
+        .toLowerCase()
+        .includes(searchField)
+    );
+    setSearchData(newfilteredProducts);
+  }, [searchField, usersData]);
+
   console.log(usersData);
   return (
     <div className="users-table-container">
       <div className="users-table-title">Greenie Users</div>
-      <SearchBox />
+      <SearchBox
+        placeholder={"search users"}
+        onSubmitHandler={onSubmitChange}
+      />
       <table className="users-table">
         <thead>
           <tr>
@@ -19,7 +40,7 @@ const UsersTable = () => {
           </tr>
         </thead>
         <tbody>
-          {usersData.map((user) => (
+          {searchData.map((user) => (
             <tr key={user.username} className="values">
               <td>{user.username}</td>
               <td>{user.email}</td>
